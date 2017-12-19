e20951767cd455a8b3787dada306e82028d844d5
juan.jusue <juanmajr97@gmail.com>
2017-12-19 11:41:26 +0100

    EXAMEN: añadido dataholder y firebaseAdmin estatico

--- examen/src/main/java/com/example/examen/DataHolder.java
+++ examen/src/main/java/com/example/examen/DataHolder.java
@@ -7,4 +7,8 @@ package com.example.examen;
 public class DataHolder {
         public static DataHolder instance= new DataHolder();
 
+        public FireBaseAdmin fireBaseAdmin;
+        public DataHolder(){
+                fireBaseAdmin = new FireBaseAdmin();
+        }
 }
